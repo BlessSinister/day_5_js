f2af06030197925a18ad50e92f72eaecc70143b0
Bless <ig.marks@yandex.ru>
2025-11-07 21:03:26 +0300

    add_method_post_get_by_id

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
 class FetchReq {
-  get(url) {
+  getAll(url) {
     fetch(url)
       .then((response) => response.json())
       .then((json) => console.log(json))
   }
+  async getTaskById(url, id) {
+    try {
+      console.log(`${url}${id}`)
+      const response = await fetch(`${url}${id}`)
+      const json = await response.json()
+      console.log(JSON.stringify(json))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  async post(url) {
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+          name: 'Сделать ДЗ',
+          info: 'Написать 2 класса',
+          isImportant: true,
+          isCompleted: false,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+      const json = await response.json()
+      console.log(JSON.stringify(json))
+    } catch (error) {
+      console.error(error)
+    }
+  }
 }
 
 let x = new FetchReq()
-x.get('https://tasks-service-maks1394.amvera.io/tasks')
+x.getTaskById('https://tasks-service-maks1394.amvera.io/tasks/', '4')
+// x.post('https://tasks-service-maks1394.amvera.io/tasks') // создал задачку с Id:4
